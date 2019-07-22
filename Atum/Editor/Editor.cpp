5fbabc51d4cae8d9efedf5726d82263faaea03c4
e.solyanov <e.solyanov>
2019-07-21 22:26:06 -0700

    restoring show of FPS

--- Atum/Editor/Editor.cpp
+++ Atum/Editor/Editor.cpp
@@ -1103,7 +1103,7 @@ void Editor::OnUpdate(EUIWidget* sender)
 
 	core.controls.SetFocused(in_scene_run ? game_viewport->IsFocused() : viewport->IsFocused());
 
-	//core.render.DebugPrintText(5.0f, COLOR_WHITE, "%i", core.GetFPS());
+	core.render.DebugPrintText(5.0f, COLOR_WHITE, "%i", core.GetFPS());
 
 	core.Update();
 

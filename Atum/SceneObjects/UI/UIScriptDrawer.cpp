f703bfecc7b286837a5f0461aa6d001e4dfa1449
e.solyanov <e.solyanov>
2020-06-02 19:17:47 -0700

    ScriptDrawer

---
+++ Atum/SceneObjects/UI/UIScriptDrawer.cpp
@@ -0,0 +1,194 @@
+#include "UIScriptDrawer.h"
+#include "SceneObjects/2D/SpriteWindow.h"
+#include "SceneObjects/2D/SpriteAsset.h"
+#include "Services/Core/Core.h"
+
+CLASSREG(UIWidgetAsset, UIScriptDrawerAsset, "ScriptDrawer")
+
+META_DATA_DESC(UIScriptDrawerAsset)
+	BASE_WIDGET_ASSET_PROP(UIScriptDrawerAsset)
+	FLOAT_PROP(UIScriptDrawerAsset, trans.pos.x, 0.0f, "Prop", "x", "X coordinate of a position")
+	FLOAT_PROP(UIScriptDrawerAsset, trans.pos.y, 0.0f, "Prop", "y", "Y coordinate of a position")
+	ENUM_PROP(UIScriptDrawerAsset, horzAlign, 0, "Prop", "horz_align", "Horizontal aligment of a widget")
+		ENUM_ELEM("Left", 0)
+		ENUM_ELEM("Center", 1)
+		ENUM_ELEM("Right", 2)
+	ENUM_END
+	ENUM_PROP(UIScriptDrawerAsset, vertAlign, 0, "Prop", "vert_align", "Vertical aligment of a widget")
+		ENUM_ELEM("Top", 3)
+		ENUM_ELEM("Center", 1)
+		ENUM_ELEM("Bottom", 4)
+	ENUM_END
+	FLOAT_PROP(UIScriptDrawerAsset, trans.size.x, 100.0f, "Prop", "width", "Width of a widget")
+	FLOAT_PROP(UIScriptDrawerAsset, trans.size.y, 100.0f, "Prop", "height", "Height of a widget")
+	FLOAT_PROP(UIScriptDrawerAsset, trans.offset.x, 0.0f, "Prop", "anchorn_x", "X coordinate of anchorn in absolute units")
+	FLOAT_PROP(UIScriptDrawerAsset, trans.offset.y, 0.0f, "Prop", "anchorn_y", "Y coordinate of anchorn in absolute units")
+	FLOAT_PROP(UIScriptDrawerAsset, rotate, 0.0f, "Prop", "rotate", "Rotation a widget")
+	COLOR_PROP(UIScriptDrawerAsset, color, COLOR_WHITE, "Prop", "color")
+	FLOAT_PROP(UIScriptDrawerAsset, color.a, 1.0f, "Prop", "alpha", "Transparancy of a widget")
+	SPRITE_PROP(UIScriptDrawerAsset, sprite, "Prop", "image")
+META_DATA_DESC_END()
+
+UIScriptDrawerAsset::~UIScriptDrawerAsset()
+{
+
+}
+
+void UIScriptDrawerAsset::Init()
+{
+}
+
+void UIScriptDrawerAsset::Draw(float dt)
+{
+	if (GetState() == Invisible)
+	{
+		return;
+	}
+
+	CalcState();
+
+	Sprite::UpdateFrame(&sprite, &frame_state, dt);
+
+#ifdef EDITOR
+	/*if (Gizmo::inst->trans2D == &trans && SpriteWindow::instance && !SpriteWindow::instance->show_anim)
+	{
+		state.cur_frame = SpriteWindow::instance->cur_frame;
+	}
+
+	if (SpriteWindow::instance)
+	{
+		SpriteWindow::instance->CheckStateOfBorder();
+	}*/
+#endif
+
+	Sprite::Draw(&trans, cur_color, &sprite, &frame_state, false, true);
+
+	UIWidgetAsset::Draw(dt);
+}
+
+CLASSREG(UIWidgetAsset, UIScriptDrawerAssetInst, "UIScriptDrawer")
+
+#ifdef EDITOR
+void StartEditUIScriptDrawerInst(void* owner)
+{
+	UIScriptDrawerAssetInst* sprite = (UIScriptDrawerAssetInst*)owner;
+	SpriteWindow::StartEdit(&sprite->sprite);
+}
+#endif
+
+META_DATA_DESC(UIScriptDrawerAssetInst)
+	BASE_WIDGET_INST_PROP(UIScriptDrawerAssetInst)
+	COLOR_PROP(UIScriptDrawerAssetInst, color, COLOR_WHITE, "Prop", "color")
+	FLOAT_PROP(UIScriptDrawerAssetInst, color.a, 1.0f, "Prop", "alpha", "Transparancy of a widget")
+#ifdef EDITOR
+	CALLBACK_PROP(UIScriptDrawerAssetInst, StartEditUIScriptDrawerInst, "Prop", "EditSprite")
+#endif
+META_DATA_DESC_END()
+
+void UIScriptDrawerAssetInst::BindClassToScript()
+{
+	const char* brief = "UI script drawer instance\n"
+		"\n"
+		"UI script drawed widget.\n"
+		"\n"
+		"This class ::UIScriptDrawerInst is a representation on C++ side.\n";
+
+	BIND_INST_TYPE_TO_SCRIPT(UIScriptDrawerAssetInst, UIScriptDrawerAsset, brief)
+	core.scripts.RegisterObjectMethod(script_class_name, "void SetSpriteFromAsset(string&in)", WRAP_MFN(UIScriptDrawerAssetInst, SetSpriteFromAsset), "Setting sprite from asset by name");
+	core.scripts.RegisterObjectMethod(script_class_name, "void Draw(Vector2&in)", WRAP_MFN(UIScriptDrawerAssetInst, AddDraw), "Add drawer");
+}
+
+void UIScriptDrawerAssetInst::SetSpriteFromAsset(string& asset_name)
+{
+	SpriteAsset* asset = dynamic_cast<SpriteAsset*>(GetScene()->FindInGroup("SpriteAsset", asset_name.c_str()));
+
+	if (asset)
+	{
+		Sprite::Copy(&asset->sprite, &sprite);
+	}
+}
+
+void UIScriptDrawerAssetInst::AddDraw(Vector2& pos)
+{
+	drawers.push_back(Drawer());
+
+	Drawer& drawer = drawers[drawers.size() - 1];
+	drawer.pos = pos;
+}
+
+void UIScriptDrawerAssetInst::Draw(float dt)
+{
+	if (GetState() == Invisible)
+	{
+		return;
+	}
+
+	CalcState();
+
+	Sprite::UpdateFrame(&sprite, &frame_state, dt);
+
+#ifdef EDITOR
+	/*if (Gizmo::inst->trans2D == &trans && SpriteWindow::instance && !SpriteWindow::instance->show_anim)
+	{
+	state.cur_frame = SpriteWindow::instance->cur_frame;
+	}
+
+	if (SpriteWindow::instance)
+	{
+	SpriteWindow::instance->CheckStateOfBorder();
+	}*/
+#endif
+
+#ifdef EDITOR
+	if (!GetScene()->Playing())
+	{
+		Sprite::Draw(&trans, cur_color, &sprite, &frame_state, false, true);
+	}
+	else
+#endif
+	{
+		auto init_pos = trans.pos;
+
+		for (auto& drawer : drawers)
+		{
+			trans.pos.x = init_pos.x + drawer.pos.x;
+			trans.pos.y = init_pos.y + drawer.pos.y;
+
+			trans.BuildMatrices();
+
+			Sprite::Draw(&trans, cur_color, &sprite, &frame_state, false, true);
+		}
+
+		drawers.clear();
+
+		trans.pos = init_pos;
+	}
+
+	UIWidgetAsset::Draw(dt);
+}
+
+#ifdef EDITOR
+UIScriptDrawerAssetInst* UIScriptDrawerAssetInst::temp = nullptr;
+
+bool UIScriptDrawerAssetInst::AddedToTreeByParent()
+{
+	return true;
+}
+
+void UIScriptDrawerAssetInst::StoreProperties()
+{
+	if (!temp)
+	{
+		temp = new UIScriptDrawerAssetInst();
+	}
+
+	GetMetaData()->Prepare(temp);
+	GetMetaData()->Copy(this);
+}
+
+void UIScriptDrawerAssetInst::RestoreProperties()
+{
+	GetMetaData()->Prepare(this);
+	GetMetaData()->Copy(temp);
+}
+#endif
---
+++ Atum/SceneObjects/UI/UIScriptDrawer.h
@@ -0,0 +1,136 @@
+#pragma once
+#include "UIWidgetAsset.h"
+
+/**
+\page scene_assets_ui_script_drawer_asset Asset
+
+UI script drawed.
+
+This class ::UIScriptDrawerAsset is a representation on C++ side.
+
+Parameters
+----------
+
+Name              | Description
+------------------|--------------
+image             | Sprite data of image
+x                 | X coordinate of a position
+y                 | Y coordinate of a position
+horz_align        | Horizontal aligment of a widget
+vert_align        | Vertical aligment of a widget
+width             | Width of a widget
+height            | Height of a widget
+horz_size         | Type of width of a widget
+vert_size         | Type of height of a widget
+anchorn_x         | X coordinate of anchorn in absolute units
+anchorn_y         | Y coordinate of anchorn in absolute units
+left_padding      | Left padding of a widget
+top_padding       | Top padding of a widget
+right_padding     | Right padding of a widget
+bottom_padding    | Bottom padding of a widget
+rotate            | Rotation a widget
+color             | Color of a widget
+alpha             | Transparancy of a widget
+scale_childs      | Should be childs sacled in case size of a widget was changed
+clip_childs       | Should be childs clipped by size of a widget
+
+*/
+
+
+/**
+\ingroup gr_code_scene_assets_ui
+*/
+
+/**
+\brief Representation of UI script drawed widget asset
+UI script drawed.
+
+*/
+
+class UIScriptDrawerAsset : public UIWidgetAsset
+{
+public:
+
+	/**
+	\brief Sprite data of image
+	*/
+
+	Sprite::Data sprite;
+
+#ifndef DOXYGEN_SKIP
+
+	META_DATA_DECL(UIScriptDrawerAsset)
+
+	Sprite::FrameState frame_state;
+
+	virtual ~UIScriptDrawerAsset();
+
+	void Init() override;
+	void Draw(float dt) override;
+
+#endif
+};
+
+/**
+\page scene_assets_ui_script_drawer_inst Instance
+
+UI script drawed widget.
+
+This class ::UIImageAssetInst is a representation on C++ side.
+
+This class Script::UIImage is a representation in a script.
+
+Parameters
+----------
+
+Name              | Description
+------------------|--------------
+color             | Color of a widget
+alpha             | Transparancy of a widget
+
+*/
+
+/**
+\ingroup gr_code_scene_objects_ui
+*/
+
+/**
+\brief Representation of UI image widget instance
+
+UI image widget.
+
+This class Script::UIImage is a representation in a script.
+
+*/
+
+class UIScriptDrawerAssetInst : public UIScriptDrawerAsset
+{
+public:
+
+	struct Drawer
+	{
+		Vector2 pos;
+	};
+
+	vector<Drawer> drawers;
+
+	META_DATA_DECL(UIScriptDrawerAssetInst)
+
+#ifndef DOXYGEN_SKIP
+
+	void Draw(float dt) override;
+
+	void BindClassToScript() override;
+	void SetSpriteFromAsset(string& asset);
+
+	void AddDraw(Vector2& pos);
+
+#ifdef EDITOR
+	static UIScriptDrawerAssetInst* temp;
+	bool AddedToTreeByParent() override;
+	void StoreProperties() override;
+	void RestoreProperties() override;
+#endif
+
+#endif
+};
